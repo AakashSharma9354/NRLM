2a18e41f74f80708b7129003f82198c46d02277d
AakashSharma9354 <aakashsharma7059099@gmail.com>
2022-09-09 22:58:50 +0530

    some changes

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,34 @@
 // $(document).ready(function(){
 //   $("#myModal").modal('show');
 // });
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+         autoplayTimeout:1000,
+              autoplayHoverPause:true,
+    margin:10,
+    loop:true,
+    autoWidth:true,
+    items:4
+})
+
+// const config = {
+//     type: 'bar',
+//     data: data,
+//     options: {
+//       plugins: {
+//         title: {
+//           display: true,
+//           text: 'Chart.js Bar Chart - Stacked'
+//         },
+//       },
+//       responsive: true,
+//       scales: {
+//         x: {
+//           stacked: true,
+//         },
+//         y: {
+//           stacked: true
+//         }
+//       }
+//     }
+//   };
--- project/js/custom.js
+++ project/js/custom.js
@@ -1,3 +1,9 @@
 // $(document).ready(function(){
 //   $("#myModal").modal('show');
 // });
+$('.owl-carousel').owlCarousel({
+    margin:10,
+    loop:true,
+    autoWidth:true,
+    items:4
+})
